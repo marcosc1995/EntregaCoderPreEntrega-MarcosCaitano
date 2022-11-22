d9bf17cb93cec7366cbd78fd965f316204aeaa16
Marcos_c95 <marcos_c95@outlook.com>
2022-11-22 20:08:45 -0300

    funcionan los endpoints de productos

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -3,6 +3,7 @@ class Products {
   constructor() {
     // this.filename = filename;
     this.products = this.readData();
+    this.admin = true;
   }
 
   async readData() {
@@ -25,10 +26,27 @@ class Products {
     let id = data.map((produc) => produc.id);
     return Math.max(...id) + 1;
   }
+  async generateDate() {
+    const tiempoTrans = Date.now();
+    const date = new Date(tiempoTrans);
+    return date;
+  }
+  async getAll() {
+    try {
+      const data = await this.readData();
+      //console.log(data);
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
   async save(product) {
     try {
       this.productos = await this.readData();
       product.id = await this.generateId();
+      const tiempoTrans = Date.now();
+      const date = new Date(tiempoTrans);
+      product.date = date.toUTCString();
       this.productos.push(product);
       this.writeData(this.productos);
       console.log(`${product.name} se guardo correctamente`);
@@ -37,7 +55,44 @@ class Products {
       console.log(e);
     }
   }
-
+  async deleteById(id) {
+    try {
+      const data = await this.readData();
+      let itemToDelete = data.filter((item) => item.id !== id);
+      this.writeData(itemToDelete);
+      console.log(`El producto con ID: ${id} se borro correctamenta`);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async getById(id) {
+    try {
+      const data = await this.products;
+      let itemToFind = data.find((item) => item.id === id);
+      //console.log(itemToFind ? itemToFind : null);
+      return itemToFind ? itemToFind : null;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async replaceById(newItem) {
+    try {
+      const data = await this.readData();
+      const newProducts = data.map((item) => {
+        if (item.id === newItem.id) {
+          item = newItem;
+          const tiempoTrans = Date.now();
+          const date = new Date(tiempoTrans);
+          item.date = date.toUTCString();
+          return item;
+        } else {
+          return item;
+        }
+      });
+      //return newProducts;
+      this.writeData(newProducts);
+    } catch (error) {}
+  }
   //   async save() {
   //     try {
   //       if (fs.existsSync(this.products)) {
@@ -53,6 +108,4 @@ class Products {
   //   }
 }
 
-
-
-module.exports = Products
+module.exports = Products;
--- routes/products/index.js
+++ routes/products/index.js
@@ -1,17 +1,40 @@
-const {Router} = require('express')
+const { Router } = require("express");
 
-const routerProducts = new Router()
-const ProductsController = require('../../controllers/products/index.js')
-const controller = new ProductsController()
+const routerProducts = new Router();
+const ProductsController = require("../../controllers/products/index.js");
+const controller = new ProductsController();
+const allProducts = controller.getAll();
 
-routerProducts.get('/', (req, res)=>{res.send('Bienvenidos a Productos')})
-routerProducts.post('/', (req, res)=>{
-    // res.send('Bienvenidos a Productos')
-    controller.save(req.body)
-    // console.log(req.body)
-    res.send('se guardo con exito')
-})
-routerProducts.put('/', (req, res)=>{res.send('Bienvenidos a Productos')})
-routerProducts.delete('/', (req, res)=>{res.send('Bienvenidos a Productos')})
+routerProducts.get("/", async (req, res) => {
+  const prod = await controller.getAll();
+  res.send(prod);
+});
+routerProducts.get("/:id", async (req, res) => {
+  const idParam = parseInt(req.params.id);
+  const product = await controller.getById(idParam);
+  res.send(product);
+});
+routerProducts.post("/", (req, res) => {
+  controller.save(req.body);
 
-module.exports = routerProducts
+  res.send("se guardo con exito");
+});
+routerProducts.put("/:id", async (req, res) => {
+  const idParam = parseInt(req.params.id);
+  const { body } = req;
+  body.id = idParam;
+  const newList = await controller.replaceById(body);
+  //controller.writeData(newList)
+  res.send(newList);
+});
+routerProducts.delete("/:id", (req, res) => {
+  try {
+    const idParam = parseInt(req.params.id);
+    const deleted = controller.deleteById(idParam);
+    res.send(deleted);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = routerProducts;
